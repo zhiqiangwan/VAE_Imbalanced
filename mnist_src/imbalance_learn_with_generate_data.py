e52c475d402ac49958ba6b8f7efbeb3c60f5885f
zhiqiangwan <zhiqiang_wan@my.uri.edu>
2017-06-07 16:06:55 -0400

    VAE for mnist dataset imbalanced learning

---
+++ mnist_src/imbalance_learn_with_generate_data.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 14:45:55 2017
+
+@author: cisa
+"""
+#standard library imports
+import os
+
+#related third party imports
+import tensorflow as tf
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import h5py
+import matplotlib.pyplot as plt
+import sklearn.metrics as sk_metric
+import pandas as pd
+
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto(log_device_placement=True)
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+#local application/library specific imports
+
+#'../data/affnist/data_50/' #
+directory_generate_data = '../data/mnist/data_50/' #'../data/affnist/data_384/' #'../data/affnist/data_128/' 
+
+GENERATE_DATA_TYPE = 'VAE' #'copy' # 'GAN'#
+vae_hidden_size = 128
+VAE_file = 'VAE_hidden_%d_generated_data.h5' % (vae_hidden_size) # 'VAE_generated_data.h5' #
+#input data processing
+input_h5 = os.path.join(directory_generate_data, 'original_data.h5')
+with h5py.File(input_h5,'r') as hf:
+    tem = hf.get('train_refined_images')
+    train_refined_images = np.array(tem) 
+    tem = hf.get('train_refined_labels')
+    train_refined_labels = np.array(tem)
+    tem = hf.get('test_refined_images')
+    test_refined_images = np.array(tem)
+    tem = hf.get('test_refined_labels')
+    test_refined_labels = np.array(tem)
+
+if GENERATE_DATA_TYPE == 'copy':
+    generate_h5 = os.path.join(directory_generate_data, 'copy_data.h5') 
+    with h5py.File(generate_h5,'r') as hf:
+        tem = hf.get('copy_images')
+        generate_images = np.array(tem)
+        tem = hf.get('copy_labels')
+        generate_labels = np.array(tem)      
+elif GENERATE_DATA_TYPE == 'VAE':    
+    generate_h5 = os.path.join(directory_generate_data, VAE_file)
+    with h5py.File(generate_h5,'r') as hf:
+        tem = hf.get('VAE_images')
+        generate_images = np.array(tem)
+        tem = hf.get('VAE_labels')
+        generate_labels = np.array(tem)    
+elif GENERATE_DATA_TYPE == 'GAN':
+    generate_h5 = os.path.join(directory_generate_data, 'GAN_generated_data.h5')
+    with h5py.File(generate_h5,'r') as hf:
+        tem = hf.get('GAN_images')
+        generate_images = np.array(tem)
+        tem = hf.get('GAN_labels')
+        generate_labels = np.array(tem)         
+
+#plt.imshow(np.reshape(generate_images[2+4600*4,:], (28, 28)))
+        
+num_classes = 10
+batch_size = 128
+epochs = 50
+CLASSIFY_TYPE = 'CNN'  #'MLP'# MLP or CNN
+
+with tf.device('/gpu:1'):
+
+    if CLASSIFY_TYPE == 'MLP':
+        image_train = np.concatenate( (train_refined_images, generate_images), axis=0)
+        label_train = keras.utils.to_categorical(np.concatenate( (train_refined_labels, generate_labels) ), num_classes)
+        image_test = test_refined_images
+        label_test = keras.utils.to_categorical(test_refined_labels, num_classes)
+        
+        model = Sequential()
+        model.add(Dense(126, activation='relu', input_shape=(784,)))
+        model.add(Dropout(0.2))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.2))     
+        model.add(Dense(num_classes, activation='softmax'))
+        model.summary()
+      
+    elif CLASSIFY_TYPE == 'CNN':    
+        image_shape_for_train = (-1, 28, 28, 1)  #   
+        image_train = np.reshape( np.concatenate( (train_refined_images, generate_images), axis=0) , image_shape_for_train)
+        label_train = keras.utils.to_categorical( np.concatenate( (train_refined_labels, generate_labels) ) , num_classes)
+        image_test = np.reshape(test_refined_images, image_shape_for_train)
+        label_test = keras.utils.to_categorical(test_refined_labels, num_classes)
+        
+        input_shape = image_shape_for_train[1:]#(28, 28, 1)       
+    
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=input_shape))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.summary()    
+    
+    
+    #plt.imshow(image_train[1100])
+    
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    
+    model.fit(image_train, label_train, batch_size=batch_size, epochs=epochs,
+              verbose=1, validation_data=(image_test, label_test))
+#score = model.evaluate(image_test, label_test, verbose=0)
+predict_output = np.argmax( model.predict(image_test), axis=1 )
+
+groundtruth_pd = pd.Series(test_refined_labels, name="groundtruth")
+pred_pd = pd.Series(predict_output, name="predicted")
+df_confusion = pd.crosstab(groundtruth_pd, pred_pd, margins=True)
+
---
+++ mnist_src/original_data.py
@@ -0,0 +1,97 @@
+'''TensorFlow implementation of http://arxiv.org/pdf/1312.6114v10.pdf'''
+
+from __future__ import absolute_import, division, print_function
+
+import math
+import os
+
+import numpy as np
+import scipy.misc
+import tensorflow as tf
+from tensorflow.contrib import layers
+from tensorflow.contrib import losses
+from tensorflow.contrib.framework import arg_scope
+from scipy.misc import imsave
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
+import h5py
+import matplotlib.pyplot as plt
+
+import keras
+from keras.datasets import cifar10
+from keras import backend as K
+
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto(log_device_placement=True)
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+import glob
+import scipy.io as spio
+
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_integer("batch_size", 128, "batch size")
+flags.DEFINE_integer("updates_per_epoch", 1000, "number of updates per epoch")
+flags.DEFINE_integer("max_epoch", 100, "max epoch")
+flags.DEFINE_float("learning_rate", 1e-2, "learning rate")
+flags.DEFINE_string("working_directory", "./", "")
+flags.DEFINE_integer("hidden_size", 128, "size of the hidden VAE unit")
+flags.DEFINE_string("model", "vae", "gan or vae")
+flags.DEFINE_string("generate_size", 2450, "batch size of generated images")
+
+FLAGS = flags.FLAGS
+
+#'../data/affnist/data_50/' #'../data/affnist/data_1152/' #
+directory_generate_data = '../data/mnist/data_50/' # '../data/affnist/data_50/label_9/' #'../data/affnist/data_128/'
+if not os.path.exists(directory_generate_data):
+    os.makedirs(directory_generate_data)
+   
+    
+##########################load MNIST data########################
+data_directory = os.path.join(FLAGS.working_directory, "MNIST")
+if not os.path.exists(data_directory):
+    os.makedirs(data_directory)
+mnist_all = input_data.read_data_sets(data_directory, one_hot=False, validation_size = 0)
+
+x_train = mnist_all.train.images
+y_train = mnist_all.train.labels
+x_test = mnist_all.test.images
+y_test = mnist_all.test.labels
+
+#plt.imshow(np.reshape(x_train[2,:], (28, 28)))
+#plt.imshow(resized_image)
+
+
+refined_label = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+num_minority_label = 50#128#50#1152#
+num_majority_label = 2500
+num_train_per_label = [num_minority_label]*5 + [num_majority_label]*5  #+ [num_majority_label]*1
+train_refined_label_idx = np.array([], dtype = np.uint8)
+test_refined_label_idx = np.array([], dtype = np.uint8)
+for idx, label_value in enumerate(refined_label):
+    refined_one_label_idx = np.where( y_train == label_value )[0][:num_train_per_label[idx]]
+    train_refined_label_idx = np.append( train_refined_label_idx,  refined_one_label_idx)
+    test_refined_one_label_idx = np.where( y_test == label_value )[0]
+    test_refined_label_idx = np.append(test_refined_label_idx, test_refined_one_label_idx)
+
+
+
+train_refined_images = x_train[train_refined_label_idx, :]
+train_refined_labels = y_train[train_refined_label_idx]
+test_refined_images = x_test[test_refined_label_idx, :]
+test_refined_labels = y_test[test_refined_label_idx]
+
+
+f = h5py.File(os.path.join(directory_generate_data, 'original_data.h5'), "w")
+f.create_dataset("train_refined_images", dtype='float32', data=train_refined_images)
+f.create_dataset("train_refined_labels", dtype='uint8', data=train_refined_labels)
+f.create_dataset("test_refined_images", dtype='float32', data=test_refined_images)
+f.create_dataset("test_refined_labels", dtype='uint8', data=test_refined_labels)
+f.close()
+
+#
+#
+#plt.imshow(np.reshape(train_refined_images[4+50*6,:], (28, 28)),)
+
